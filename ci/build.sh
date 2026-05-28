93ed5ac6619f1781c62eb44d27ce81a0f8d5b54f
Benjamin Bannier <benjamin.bannier@corelight.com>
2026-05-28 10:35:26 +0200

    fixup! Infer default value for ZEEK_CI_CPUS

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
-ZEEK_CI_CPUS=${ZEEK_CI_CPUS:-$(nproc)}
+set -x
+
+# NOTE: We do not use `nproc` since on some platforms it needs to be explicitly
+# installed while `getconf` is POSIX.
+ZEEK_CI_CPUS=${ZEEK_CI_CPUS:-$(getconf _NPROCESSORS_ONLN)}
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
